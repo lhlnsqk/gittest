b671cda6428c30aff8a4bf4c70cff41f0e4a604d
lhl <10696@qq.com>
2021-07-19 23:52:02 +0800

    修改了方法，添加注释

--- git-test/src/com/lhl/git/git.java
+++ git-test/src/com/lhl/git/git.java
@@ -4,21 +4,13 @@ public class git {
 
 
     public static void main(String[] args) {
-        System.out.println("hello git!!!!!");
-        System.out.println("hello git!!!!!");
-        System.out.println("hello git!!!!!");
-        System.out.println("hello git!!!!!");
-        System.out.println("hello git!!!!!");
-        System.out.println("hello git!!!!!");
-        System.out.println("hello git!!!!!");
+        //hello git!!!!
         System.out.println("hello git!!!!!");
         System.out.println("hello git!!!!!");
     }
 
     public void test(){
-        System.out.println("test!!!!");
-        System.out.println("test!!!!");
-        System.out.println("test!!!!");
+        //hello test!!!!
         System.out.println("test!!!!");
     }
 }
