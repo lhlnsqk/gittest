775b28bf23de299214c3bbd963bf3b8eccd7e560
lhl <10696@qq.com>
2021-07-19 23:06:47 +0800

    把本地工程添加到git

---
+++ git-test/src/com/lhl/git/git.java
@@ -0,0 +1,17 @@
+package com.lhl.git;
+
+public class git {
+
+
+    public static void main(String[] args) {
+        System.out.println("hello git!!!!!");
+        System.out.println("hello git!!!!!");
+        System.out.println("hello git!!!!!");
+        System.out.println("hello git!!!!!");
+        System.out.println("hello git!!!!!");
+        System.out.println("hello git!!!!!");
+        System.out.println("hello git!!!!!");
+        System.out.println("hello git!!!!!");
+        System.out.println("hello git!!!!!");
+    }
+}
